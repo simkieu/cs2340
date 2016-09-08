8bcb0acf7cf9bb4a2a6e7034373fc205f0a161f5
ngailapdi <ngailapdi@gmail.com>
2016-09-08 11:47:56 -0400

    Change Person5.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,9 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+	  if (input.length() <= 2) return input;
+     String result = input.substring(2) + input.substring(0,2);
+	  return result;
 	}
 	
 	/**
