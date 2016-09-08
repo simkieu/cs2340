62074f92c51bdf232cb62457d611929b5c4ec166
Sim Kieu <simkieu@gmail.com>
2016-09-08 11:52:28 -0400

    I changed Person1.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,9 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		if (input.length() <= 2)
+			return input;
+		return input.substring(2) + input.substring(0, 2);
 	}
 	
 	/**
